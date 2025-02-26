89986a4011edfec2192baea9f2271c705fba6af8
newtsarthur <tutuhoficial13@gmail.com>
2025-02-26 15:57:45 -0300

    textos adicionados

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Game {
   public static void main(String[] args) {
     clear();
+    Date dataHoraAtual = new Date();
+    
     Random rand = new Random();
     Scanner ler = new Scanner(System.in);
+
+    String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
+    String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
 
     int tree = rand.nextInt(101);
 
@@ -18,16 +25,21 @@ public class Game {
     try {
       while(!loop) {
         if(tent < vida) {
-          System.out.println("Você tem " + (vida - tent) + " tentativas");
+          if(tent == 0){
+            System.out.println("O marciano fugiu do seu planeta natal e se escondeu atrás de uma árvore, é sua missão achar ele! Ou a Terra vai explodir!");
+          }
+          System.out.println("Você tem " + (vida - tent) + " tentativas!");
           System.out.print("Digite um valor: ");
           int chute = ler.nextInt();
           tent += 1;
           clear();
           if(chute == tree) {
             if(tent == 1) {
-              System.out.println("Parabéns, você achou com " + tent + " tentativa!");
+              System.out.println("Parabéns, você achou ele com " + tent + " tentativa!");
+              System.out.println("TerraSalva("+ data+ " às " +hora+");");
             } else {
-              System.out.println("Parabéns, você achou com " + tent + " tentativas!");
+              System.out.println("Parabéns, você achou ele com " + tent + " tentativas!");
+              System.out.println("TerraSalva("+ data+ " às " +hora+");");
             }
             loop = true;
           }
@@ -40,10 +52,14 @@ public class Game {
           
         }else {
           clear();
+          tent = 0;
+          System.out.println("Você fracassou!");
           System.out.println("O marciano estava na árvore: " + tree);
+          System.out.println("TerraExplode();");
+          System.out.println("TerraReinicia("+ data+ " às " +hora+");");
+          System.out.println("");
           tree = rand.nextInt(101);
           loop = false;
-          tent = 0;
         }
       }
     } catch (Exception e) {
